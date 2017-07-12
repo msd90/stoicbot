e1c99ca76197da615606eff4eff139987d24899f
msd90 <majordhaliwal90@gmail.com>
2017-07-12 07:14:34 -0600

    picks quote at random from the text file

--- hello.py
+++ hello.py
@@ -1,5 +1,6 @@
 import tweepy
 import time
+import random
 
 
 consumer_key = 'insert here'
@@ -11,35 +12,20 @@ auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_token_secret)
 
 api = tweepy.API(auth)
-#count=0
-#while True:
-#    api.update_status(count)
-#    count = count + 1
-#    time.sleep(60)
+
+#takes in text file and numerates the text file lines
+#returns line with text at random
+def random_line(afile):
+    line = next(afile)
+    for num, aline in enumerate(afile):
+      if random.randrange(num + 2): continue
+      line = aline
+    return line
 
 file = open('quotes.txt', 'r')
-i=0
 while (True):
-    tweet = file.readline()
+    tweet = random_line(file)
     api.update_status(tweet)
     #print tweet
-    time.sleep(86400)
-    i+=1
-    if i == 75:
-        file.seek(0)
-        i = 0
-        #print "done"
-
-
-
-#chars = words = lines = 0
-#file = 'quotes_test.txt'
-#with open(file, 'r') as in_file:
-#    for line in in_file:
-#        lines += 1
-#        chars += len(line)
-        #print lines, chars
-#        if (chars <= 140):
-#            print line
-#        time.sleep(2)
-#        chars = 0
+    time.sleep(43200)
+    file.seek(0)
